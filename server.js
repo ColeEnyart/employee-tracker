18f55736cb3f76bc21a640d92e3bbd0316ca64f1
Cole Enyart <letchro@gmail.com>
2021-10-24 02:44:28 -0500

    Added initial questions for inquirer to ask

--- server.js
+++ server.js
@@ -2,12 +2,36 @@ const mysql = require("mysql12");
 const inquirer = require("inquirer");
 const table = require("console.table");
 
-const db = mysql.creatConnection (
+const db = mysql.creatConnection(
     {
         host: 'localhost',
         user: 'root',
         password: '',
         database: 'employee_db'
-      },
-      console.log(`Connected to the database.`)
+    },
+    console.log(`Connected to the database.`)
 );
+
+// Initial questions to ask
+const initQuestions = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'options',
+            message: 'What would you like to do?',
+            choices: [
+                'View All Employees',
+                'Add Employee',
+                'Update Employee Role',
+                'View All Roles',
+                'Add Role',
+                'View All Departments',
+                'Add Department',
+                'Quit'
+            ]
+        }
+    ])
+
+}
+
+initQuestions();
